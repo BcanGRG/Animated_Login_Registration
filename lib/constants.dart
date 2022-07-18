e6b0d40bb9ba08c134e206840e51b295e7f16016
Burak Can <BurakCan_GRG@hotmail.com>
2022-07-18 17:30:29 +0300

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  //* Colors
+  static const Color login_bg = Color(0xFF00C470);
+  static const Color signup_bg = Color(0xFF000A54);
+
+  //* Values
+  static const double defaultPadding = 16.0;
+  static const Duration defaultDuration = Duration(milliseconds: 300);
+}
